a207f5403812b151d886f39053ed7334a94dadc4
SMXSUTR <samymar0921@gmail.com>
2026-09-18 13:44:17 -0500

    Delete secuencial8.py

--- secuencial8.py
+++
@@ -1,4 +0,0 @@
-base=float(input("Dime la base de tu rectangulo: "))
-altura=float(input("Dime la altura de tu rectangulo: "))
-perimetro=(base+altura)*2
-print(f"si la base es {base} y la altura es {altura}, entonces su perimetro es {perimetro}")
