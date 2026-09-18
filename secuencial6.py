a69a5cd4c397fdaa590c1826f2b33c4fb290f4d1
SMXSUTR <samymar0921@gmail.com>
2026-09-18 13:43:58 -0500

    Delete secuencial6.py

--- secuencial6.py
+++
@@ -1,5 +0,0 @@
-articulo=input("Que articulo quieres llevar? ")
-cantidad=int(input("Cuantos vas a llevar? "))
-precio=int(input("El precio individual es?"))
-total=cantidad*precio
-print(f"La cantidad total de {articulo} es {total}")
